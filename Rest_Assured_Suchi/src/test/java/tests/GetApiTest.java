5f8a019ab60782d15361296f5d295b32d9ee74bc
Vignesh Srinivasa Raghavan <vigneshsrinivasan18@gmail.com>
2024-12-18 07:06:06 +0100

    Added Put and Delete cases

--- Rest_Assured_Suchi/src/test/java/tests/GetApiTest.java
+++ Rest_Assured_Suchi/src/test/java/tests/GetApiTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class GetApiTest {
 
@@ -57,6 +58,81 @@ public class GetApiTest {
               .log().all()
               .extract()
               .response();
+   }
+
+
+   @Test
+   public  void validatePutApi(){
+      RestAssured.baseURI="https://restful-booker.herokuapp.com";
+      String requestBody = """
+              {
+               "firstname" : "Shravya",
+                  "lastname" : "Vignesh",
+                   "totalprice": 111,
+                      "depositpaid": true,
+                      "bookingdates": {
+                          "checkin": "2018-01-01",
+                          "checkout": "2019-01-01"
+                      },
+                      "additionalneeds": "Lunch"
+              }
+              """;
+
+      Response response = given()
+              .header("Content-Type", "application/json")
+              .header("Cookie","token="+getAuthToken())
+              .body(requestBody)
+              .when()
+              .put("/booking/66")
+              .then()
+              .statusCode(200)
+              .body("firstname",equalTo("Shravya"))
+              .body("lastname",equalTo("Vignesh"))
+              .body("depositpaid",equalTo(true))
+              .log().all()
+              .extract()
+              .response();
+   }
+
+   @Test
+   public  void validateDeleteApi(){
+      RestAssured.baseURI="https://restful-booker.herokuapp.com";
+
+      Response response = given()
+              .header("Content-Type", "application/json")
+              .header("Cookie","token="+getAuthToken())
+              .when()
+              .delete("/booking/66")
+              .then()
+              .statusCode(201)
+              .log().all()
+              .extract()
+              .response();
+   }
+
+
+   public String getAuthToken(){
+      RestAssured.baseURI="https://restful-booker.herokuapp.com";
+
+      String requestBody = """
+              {
+                  "username" : "admin",
+                  "password" : "password123"
+              }
+              """;
+
+      Response response = given()
+              .header("Content-Type", "application/json")
+              .body(requestBody)
+              .when()
+              .post("/auth")
+              .then()
+              .statusCode(200)
+              .body("token",notNullValue())
+              .extract()
+              .response();
+
+       return response.jsonPath().getString("token");
    }
 
 }
